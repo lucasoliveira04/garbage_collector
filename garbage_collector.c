cc7fe9be4cf934f7e14f2e9712bc5e246c486d64
Lucas Oliveira <camposdlucasoli@gmail.com>
2026-09-12 11:55:03 -0300

    feat: added function to register malloc for zone

--- garbage_collector.c
+++ garbage_collector.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// gc_malloc: Allocates memory and tracks It for garbage collection
-// gc_free: Free zone manually of memory
-typedef struct Allocator{
+typedef struct Allocator {
     void *endereco;
     size_t tamanho;
     struct Allocator *next;
@@ -12,18 +10,20 @@ typedef struct Allocator{
 
 static Allocator *allocations = NULL;
 
+// prototipos
+void *gc_malloc(size_t size);
+void gc_free(void *ptr);
+void gc_register_allocation(void *ptr, size_t size);
+void print_hp(void);
+
 void *gc_malloc(size_t size) {
     void *ptr = malloc(size);
 
-    if (ptr == NULL) return NULL;
+    if (ptr == NULL) {
+        return NULL;
+    }
 
-    Allocator *alloc = malloc(sizeof(Allocator));
-
-    alloc->endereco = ptr;
-    alloc->tamanho = size;
-    alloc->next = allocations;
-
-    allocations = alloc;
+    gc_register_allocation(ptr, size);
 
     return ptr;
 }
@@ -38,44 +38,63 @@ void gc_free(void *ptr) {
 
     while (curr != NULL) {
         if (curr->endereco == ptr) {
+
             if (prev == NULL) {
                 allocations = curr->next;
             } else {
                 prev->next = curr->next;
             }
+
+            free(curr->endereco);
+            free(curr);
+
+            return;
         }
 
-        free(curr->endereco);
-        free(curr);
+        prev = curr;
+        curr = curr->next;
+    }
+}
 
+
+void gc_register_allocation(void *ptr, size_t size) {
+    Allocator *alloc = malloc(sizeof(Allocator));
+
+    if (alloc == NULL) {
         return;
     }
 
-    prev = curr;
-    curr = curr->next;
-    printf("Memory heap after free");
-} 
+    alloc->endereco = ptr;
+    alloc->tamanho = size;
+    alloc->next = allocations;
+
+    allocations = alloc;
+}
+
 
 void print_hp(void) {
     Allocator *atual = allocations;
 
-    printf("Alocated memory heap:\n");
+    printf("Allocated memory heap:\n");
 
     while (atual != NULL) {
-        printf("Address: %p, Size: %zu\n", atual->endereco, atual->tamanho);
+        printf(
+            "Address: %p, Size: %zu\n",
+            atual->endereco,
+            atual->tamanho
+        );
+
         atual = atual->next;
     }
 }
 
 
-int main() {
+int main(void) {
     int *number = gc_malloc(22);
 
     print_hp();
 
     gc_free(number);
-
-    print_hp();
 
     return 0;
 }
